0c1f8a2045d1cd4af3991760251f54b128a7537a
alejandro9702 <ovejod12@hotmail.com>
2021-07-30 09:28:53 -0500

    componentes base para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './componentes/products/products.component';
+import { NewsComponent } from './componentes/news/news.component';
+import { CategoriesComponent } from './componentes/categories/categories.component';
+import { AdministratorsComponent } from './componentes/administrators/administrators.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NewsComponent,
+    CategoriesComponent,
+    AdministratorsComponent
   ],
   imports: [
     BrowserModule
